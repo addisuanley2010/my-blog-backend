8b60ff3b042853b71d9af9ef758e47269462dbb8
addisuanley2010 <addisuanley2005@gmail.com>
2024-04-17 08:10:56 +0300

    image API fixed

--- cloudinary.js
+++ cloudinary.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 
 // Configure Cloudinary with your API credentials
 cloudinary.config({
-  CLOUD_NAME: process.env.CLOUD_NAME,
-  API_KEY: process.env.API_KEY,
-  API_SECRET: process.env.API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
 });
 
 module.exports = cloudinary;
